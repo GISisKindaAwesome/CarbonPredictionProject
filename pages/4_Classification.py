90825aa277b1e66019c2c90212f0631bb528cfe8
Thijme <th.jonckers@gmail.com>
2026-01-15 11:14:18 +0200

    Update 4_Classification.py

--- pages/4_Classification.py
+++ pages/4_Classification.py
@@ -11,14 +11,14 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 from matplotlib.patches import Patch
 
 st.title("Classification")
-st.write("To be able to predict the amount of Carbon stored on the forests of Samos a forest classification map is needed. This then can be used for area calculation and other calculations. Copernicus provides a classification map called Corine Land Cover which could be used. However, this map will be less precise due to it being more general for the world and is from 2018. No other classification maps/data exists for the island of Samos. So for more recent and a more accurate classification it was decided to make a forest classification map based on Sentinel 2 imagery. Various classification models exist, but it was opted to use the random forest algorithmn. This algorithmn was chosen since it has high dimensionality so can process the high amount of bands and reduce noise bias. It can also capture complex boundaries better which is case for the forests here on Samos.")
-st.write("Most of the steps on this page is done with use of the Google Earth Engine API in Python.")
+st.write("To be able to predict the amount of Carbon stored on the forests of Samos a forest classification map is needed. This can then be used for area calculation and other calculations. Copernicus provides a classification map called Corine Land Cover which could have been used. However, this map would have been less precise, as it  shows more general world coverage and originates  from 2018. No other classification maps/data exists for the island of Samos, so  for a more recent and a more accurate classification it was decided to make a new forest classification map based on Sentinel 2 imagery. Although various classification models do exist,a random forest algorithm was chosen instead, because it has high dimensionality and can thus  process the high amount of bands while  reducing noise bias. It can also capture complex boundaries better, which is particularly useful  for the forests on Samos.")
+st.write("Most of the steps on this page were done with the use of the Google Earth Engine API in Python.")
 
 st.space(size="small")
 
 st.subheader("Gathering imagery")
 st.write("""
-         For the gathering of the imagery the Sentinel 2 Surface Reflectance(SR) was used. The following parameters were used:
+         For the gathering of the imagery the Sentinel 2 Surface Reflectance(SR) was used with the following parameters: 
          - Start date: 01-03-2024
          - End date: 28-02-2025
          - Maximum cloud coverage: 40%
@@ -27,14 +27,14 @@ st.write("""
 st.space(size="small")
 
 st.subheader("Apply cloud mask")
-st.write("It is almost impossible to get cloud free imagery and to still get a good temporal extent it's important to apply a cloud mask. For that the following tutorial was used: https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless. This uses the S2 cloud probability dataset (s2cloudless) together with various thresholds to be able to mask out clouds and their shadows. These thresholds can be changed but for this project the default thresholds are used.")
+st.write("It is almost impossible to get cloud free imagery and to still get a good temporal extent that's why it's important to apply a cloud mask. For that the following tutorial was used for the cloud mask: https://developers.google.com/earth-engine/tutorials/community/sentinel-2-s2cloudless. The tutorial uses the S2 cloud probability dataset (s2cloudless), together with various thresholds to mask out clouds and their shadows. These thresholds can be changed, but for this project the default thresholds were used.")
 
 st.space(size="small")
 
 st.subheader("Compositing imagery")
 st.write("""
-         With the gathered and masked images composites can be made for each season. This is done to keep the processing time lower and there is no need for so many images for classification. A lot of differences can be found in the seasons so the images are composited based on the seasons. Following a median composite to remove any outliers found in the images.
-         The seasons range from:
+         With the gathered and masked images, composites were made for each season. This was done to keep the processing time lower, since there was no need for more images for classification. As a lot of differences could be found throughout the seasons, the images were composited based separately per season, following a median composite to remove any outliers.
+         The seasons ranged from:
          - Spring: March till May 
          - Summer: June till August
          - Autumn: September till November
@@ -43,8 +43,8 @@ st.write("""
 
 st.space(size="small")
 
-st.subheader("Indices & Tassled Cap Transformation")
-st.write("To be able to have a more accurate classification various indices and the tassled cap were calculated on the seasonal composites to be able to come to conclusions. Before being able to calculate these they the SWIR bands needed to be resampled to 10 meters.These are the following:")
+st.subheader("Indices & Tasseled Cap Transformation")
+st.write("To have a more accurate classification, various indices and the tasseled cap were calculated using the seasonal composites Before being able to calculate these, the SWIR bands had to be resampled to ten meters.These indices are the following:")
 st.write("""
         - Normalized Difference Vegetation Index (NDVI)
             - Measures vegetation health and density
@@ -62,12 +62,12 @@ st.write("""
             - Measures vegetation greenness while correcting for the brightness of the underlying soil
             - Formula: (NIR-Red)*(1+L)/NIR+Red+L
                 - L is the soil adjustment factor where 0.5 is the default and can be changed per area of interest
-        - Tassled Cap Transformation
-            - This is a spectral analysis technique which converts spectral data into three components which singnify different properties.
-            - Every sattelite has different coefficients which need to be used to apply the transformation correctly. For this project the following research was used to get these coefficients for Sentinel 2:
+        - Tasseled Cap Transformation
+            - This is a spectral analysis technique which converts spectral data into three components which signify differen properties.
+            - Every satellite has different coefficients which need to be used to apply the transformation correctly. For this project the following research was used to get the coefficients for Sentinel 2:
                 - https://ieeexplore.ieee.org/document/8836649
             - The three components are as following:
-                - To be able to calculate the components firstly the values need to be converted to true reflectance values. This is done by multiplying it by 0.0001
+                - To calculate the components, the values first need to be converted to true reflectance values. This was done by multiplying it by 0.0001
                 - Greenness
                     - Measures vegetation health and density
                     - Formula: -0.2848*Blue-0.2435*Green-0.5436*Red+0.7243*NIR+0.0840*SWIR1-0.1800*SWIR2
@@ -82,7 +82,7 @@ st.write("""
 st.space(size="small")
 
 st.subheader("Training samples")
-st.write("Since the classification model is a supervised model, features are needed to be able to distinguish between classes. In this way the model has something to base it's prediction on. Based on imagery the following features were chosen:")
+st.write("Since the classification model is a supervised model, features are needed to distinguish between classes, so that the model has something to base its prediction on. Based on imagery the following features were chosen:")
 training_samples = leafmap.Map(
     zoom_control=True, 
     attribution_control=False,   
@@ -106,14 +106,14 @@ training_samples.add_shp(training_samples_file,
           layer_name="Training samples",
           style = sample_style)  
 training_samples.to_streamlit()
-st.write("Based on these features they could be split into a random 70:30 training split. Where 70% of the data will be used for the training of the random forest model and 30% will be used for the testing of the model.")
+st.write("These features were then  split into a random 70:30 training split, where 70% of the data was used for the training of the random forest model and 30% was  used for the testing of the model.")
 
 st.space(size="small")
 
 st.subheader("Vegetation mask")
-st.write("To only classify the forests a mask needs to be applied so only the forest will be classified. For this various summer indices with thresholds were used to be able to have the forests as the remaining pixels. They are as following:")
+st.write("In order to only classify the forests, a mask needed to be applied. This was done by using various summer indices with thresholds, so that only the forests would remain as the remaining pixels. This vegetation mask was set up as following:")
 st.write("""
-         Everything that fall under these thresholds will be masked out
+         Everything that fell under these thresholds was masked out:
          - NDVI: <0.70
          - NDBI: >0
          - NDWI: >-0.3
@@ -124,8 +124,8 @@ st.write("""
 st.space(size="small")
 
 st.subheader("Accuracy score")
-st.write("After running the classification on the training samples different accuracy tests can be used based on the training and test samples.")
-st.write("The proportion of classifications (total accuracy) that were correct is 0.9927 so 99.27%")
+st.write("After running the classification on the training samples, different accuracy tests were applied to the training and test samples.")
+st.write("The proportion of classifications (total accuracy) that were correct was 0.9927, or99.27%")
 st.write("The table below (confusion matrix) shows the classification model's predicted results against the actual outcomes:")
 con_matrix = [[0, 0, 0],
                [0, 1413, 3],
@@ -141,7 +141,7 @@ st.dataframe(con_df)
 st.space(size="small")
          
 st.subheader("Final classification map")
-st.write("The random forest classification leads to this map as a final result:")
+st.write("The random forest classification led to this map as a final result:")
 
 #---Interactive map but raster doesn't show up----
 #classification = leafmap.Map(
@@ -235,6 +235,7 @@ st.page_link(
     "pages/5_Total_carbon_stored.py",
     label="-> Carbon prediction"
 )
+
 
 
 
